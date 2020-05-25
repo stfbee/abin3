1c4eb46e90590ac4351aef9cf0426646060603bd
Vladislav Onishchenko <vonishchenko@croc.ru>
2020-05-25 22:39:58 +0300

    починил генерацию хедера

--- src/main/kotlin/ru/ovm/abin/App.kt
+++ src/main/kotlin/ru/ovm/abin/App.kt
@@ -12,13 +12,12 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.jackson.jackson
 import io.ktor.response.respond
+import io.ktor.response.respondFile
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import ru.ovm.abin.db.get5Albums
-import ru.ovm.abin.db.get5GroupsWithAlbums
-import ru.ovm.abin.db.get5Items
-import ru.ovm.abin.db.get5Sllers
+import ru.ovm.abin.db.*
 import ru.ovm.abin.db.utils.DatabaseFactory
+import java.io.File
 
 object App {
     lateinit var vk: VkApiClient
@@ -27,7 +26,7 @@ object App {
 
     var admin: String? = ""
     var group_id = 0
-    var version = "2.1.0"
+    var version = "3.0.0"
 
     /**
      * Минимальный размер описания, при котором он начинает считаться описанием.
@@ -53,5 +52,10 @@ fun Application.module() {
         get("/api/groups/") { call.respond(get5GroupsWithAlbums()) }
         get("/api/albums/") { call.respond(get5Albums()) }
         get("/api/items/") { call.respond(get5Items()) }
+        get("/api/draw/") {
+            val pathname = "image.png"
+            VkHeaderGenerator().drawText(getActiveGroupsCount().toString(), getActiveAlbumsCount().toString(), getActiveItemsCount().toString(), pathname)
+            call.respondFile(File(pathname))
+        }
     }
 }
--- src/main/kotlin/ru/ovm/abin/db/utils/DatabaseFactory.kt
+++ src/main/kotlin/ru/ovm/abin/db/utils/DatabaseFactory.kt
@@ -15,7 +15,7 @@ object DatabaseFactory {
         config.username = "sa"
         config.password = ""
         config.driverClassName = "org.h2.Driver"
-        config.jdbcUrl = "jdbc:h2:file:./abin.ru.ovm.abin.db;DB_CLOSE_ON_EXIT=FALSE;AUTO_SERVER=TRUE"
+        config.jdbcUrl = "jdbc:h2:file:./abin.db;DB_CLOSE_ON_EXIT=FALSE;AUTO_SERVER=TRUE"
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
